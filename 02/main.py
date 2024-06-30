c07a033ea8be561fd3b5aa62b120088d04c47b9e
Cristhian <cristhianclx@gmail.com>
2024-06-30 11:32:54 -0500

    02

--- 02/main.py
+++ 02/main.py
@@ -13,7 +13,15 @@ def index():
 
 @app.route("/data")
 def data():
-    r = requests.get("https://cuantoestaeldolar.pe/")
+    r = requests.get("https://cuantoestaeldolar.pe/", headers = {
+        "Host": "cuantoestaeldolar.pe",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.8,es-ES;q=0.5,es;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "DNT": "1",
+        "Connection": "keep-alive",
+    })
     raw = r.text
     soup = BeautifulSoup(raw, "html.parser")
     elements = soup.find_all("p", class_="ValueQuotation_text___mR_0")
